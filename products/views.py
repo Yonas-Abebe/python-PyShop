7e95be6008a60ca8bea4db35ad8328c3e9ce3536
Yonas Abebe <yonexo78@gmail.com>
2025-12-01 23:39:30 -0500

    real update

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 #URL = Uniform Resource Locator (meaning, address)
 #producs -> index //Map /products with index
 def index(request):
-    return HttpResponse("Hello Friends! Greetings from Django | Now on a server! | DEV Yoni | Python!")
+    return HttpResponse("Hello Friends! Greetings from Django | Now on a server! Very Cool!! | DEV Yoni | Python!")
 
 
 def new(request):
