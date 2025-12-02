d8d948baf82f438608822f5e53a7d15aa7669612
Yonas Abebe <yonexo78@gmail.com>
2025-12-01 22:27:50 -0500

    Initial commit

---
+++ products/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+#URL = Uniform Resource Locator (meaning, address)
+#producs -> index //Map /products with index
+def index(request):
+    return HttpResponse("Hello Friends! Greetings from Django | Now on a server! | DEV Yoni | Python!")
+
+
+def new(request):
+    return HttpResponse("New Products added | DEV Yoni | Python!")
