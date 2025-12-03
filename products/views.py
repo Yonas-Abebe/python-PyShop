38a12dc9ec5a1e947f120337f5c0a28d41b9a971
Yonas Abebe <yonexo78@gmail.com>
2025-12-03 15:07:12 -0500

    minor change1

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Friends! Greetings from Django | Now on an http server! | DEV Yoni | Python!")
+    return HttpResponse("Hello Friends! Greetings from Django | from http server! | DEV Yoni | Python!")
 
 def new(request):
     return HttpResponse("New Products added | DEV Yoni | Python!")
