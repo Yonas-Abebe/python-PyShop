94e08696b9f98ea1f7e036283886bdb487756b5f
Yonas Abebe <yonexo78@gmail.com>
2025-12-03 16:51:33 -0500

    minor change3

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from products import views   # <-- Add this import
-from .deploy import deploy
-
+from products import views   # import index & new views
+from products.views import deploy   # import deploy view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('', views.index, name='home'),
-    path('deploy/', deploy),  # <-- add this
+    path('deploy/', deploy),
 ]
