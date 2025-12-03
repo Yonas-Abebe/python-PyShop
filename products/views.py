9f01e201fa1e3df79cc58c1f80e59df4ff082458
Yonas Abebe <yonexo78@gmail.com>
2025-12-03 13:41:19 -0500

    minor change

--- products/views.py
+++ products/views.py
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-#URL = Uniform Resource Locator (meaning, address)
-#producs -> index //Map /products with index
-def index(request):
-    return HttpResponse("Hello Friends! Greetings from Django | Now on a server! Very Cool!! | DEV Yoni | Python!")
-
-
-def new(request):
-    return HttpResponse("New Products added | DEV Yoni | Python!")
-=======
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Friends! Greetings from Django | Now on a server! | DEV Yoni | Python!")
+    return HttpResponse("Hello Friends! Greetings from Django | Now on an http server! | DEV Yoni | Python!")
 
 def new(request):
     return HttpResponse("New Products added | DEV Yoni | Python!")
->>>>>>> 28523f3dfcc043cc39b382f9a5758251ff77d63c
